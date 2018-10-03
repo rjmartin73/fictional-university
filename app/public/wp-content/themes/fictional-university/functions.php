3a5770d5c91a68db36c216f7505a9d249c601720
Ryan Martin <rjmartin73@gmail.com>
2018-10-02 19:24:08 -0600

    Updates

--- app/public/wp-content/themes/fictional-university/functions.php
+++ app/public/wp-content/themes/fictional-university/functions.php
@@ -6,6 +6,5 @@
         wp_enqueue_style('font-awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         wp_enqueue_style('university_mail_styles',get_stylesheet_uri());
     }  
-    
-        add_action('wp_enqueue_scripts','university_files');
+            add_action('wp_enqueue_scripts','university_files');
 ?>
