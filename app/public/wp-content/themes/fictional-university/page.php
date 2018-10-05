aa90fd6ced739d3f96c01ecfb01cd35fa83fd4c0
Ryan Martin <rjmartin73@gmail.com>
2018-10-04 20:34:15 -0600

    Formatting updates

--- app/public/wp-content/themes/fictional-university/page.php
+++ app/public/wp-content/themes/fictional-university/page.php
@@ -1,11 +1,11 @@
-
 <?php
-    get_header();
+get_header();
 
-    while(have_posts()) {
-       the_post(); ?>
+while (have_posts()) {
+  the_post();
+  ?>
        
-       <div class="page-banner">
+  <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title() ?></h1>
@@ -18,7 +18,9 @@
   <div class="container container--narrow page-section">
 
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p><a class="metabox__blog-home-link" href="#">
+        <i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> 
+        <span class="metabox__main">Our History</span></p>
     </div>
     
   <!--  
@@ -36,7 +38,8 @@
 
   </div>
         
-    <?php } 
-    get_footer();
-    ?>
+    <?php 
+  }
+  get_footer();
+  ?>
 
