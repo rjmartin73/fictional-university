5ea34fefa2c1ef11589893ea2867eae9d9bbb14f
Ryan Martin <rjmartin73@gmail.com>
2018-10-04 20:56:49 -0600

    Finish page template
    Page template lesson

--- app/public/wp-content/themes/fictional-university/page.php
+++ app/public/wp-content/themes/fictional-university/page.php
@@ -6,11 +6,12 @@ while (have_posts()) {
   ?>
        
   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+    <div class="page-banner__bg-image" 
+      style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title() ?></h1>
       <div class="page-banner__intro">
-        <p>DON'T FORGET TO REPLACE ME LATER</p>
+        <p>***** DON'T FORGET TO REPLACE ME LATER *****</p>
       </div>
     </div>  
   </div>
@@ -23,7 +24,7 @@ while (have_posts()) {
         <span class="metabox__main">Our History</span></p>
     </div>
     
-  <!--  
+    <!--
       <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
@@ -32,6 +33,7 @@ while (have_posts()) {
       </ul>
     </div>
     -->
+
     <div class="generic-content">
       <?php the_content(); ?>
       </div>
