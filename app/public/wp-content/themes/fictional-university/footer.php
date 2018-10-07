1c5588f05cb9562d528fd0754683bf76e315eb10
Ryan Martin <rjmartin73@gmail.com>
2018-10-07 17:54:36 -0600

    20181007 Commit

--- app/public/wp-content/themes/fictional-university/footer.php
+++ app/public/wp-content/themes/fictional-university/footer.php
@@ -37,15 +37,7 @@
 
     <div class="site-footer__col-four">
       <h3 class="headline headline--small">Connect With Us</h3>
-      <nav>
-        <ul class="min-list social-icons-list group">
-          <li><a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-          <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-          <li><a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-          <li><a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-          <li><a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-        </ul>
-      </nav>
+      <nav>git 
     </div>
   </div>
 
