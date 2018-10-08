14b3def34b26072cfb0011b46b865c4bab74c50a
Ryan Martin <rjmartin73@gmail.com>
2018-10-08 13:35:34 -0600

    Revert "After child pages"
    This reverts commit 7cc318330a5541756d0dd432f018f1e513c6fbd1.

--- app/public/wp-content/themes/fictional-university/page.php
+++ app/public/wp-content/themes/fictional-university/page.php
@@ -18,18 +18,12 @@ while (have_posts()) {
 
   <div class="container container--narrow page-section">
 
-    <?php 
-    $the_parent = wp_get_post_parent_id(get_the_ID());
-    if ($the_parent) { ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent); ?>">
-        <i class="fa fa-home" aria-hidden="true"></i> Back to 
-        <?php echo get_the_title($the_parent); ?></a> 
-        <span class="metabox__main"><?php the_title(); ?></span></p>
+      <p><a class="metabox__blog-home-link" href="#">
+        <i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> 
+        <span class="metabox__main">Our History</span></p>
     </div>
-    <?php 
-  }
-  ?>
+    
     <!--
       <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
