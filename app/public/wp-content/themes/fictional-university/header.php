027360cf1bdaa68722c2d450dce9adeffa864417
Ryan Martin <rjmartin73@gmail.com>
2018-10-09 19:12:00 -0600

    Chapter 5 lesson 17

--- app/public/wp-content/themes/fictional-university/header.php
+++ app/public/wp-content/themes/fictional-university/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
+      <meta charset="<?php bloginfo('charset'); ?>">
+      <meta name="viewport" content="width=device-width,inital-scale=1">
       <?php wp_head();
       //wp_admin_bar_sidebar_toggle($wp_admin_bar); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="site-header">
     <div class="container">
       <h1 class="school-logo-text float-left"><a href="<?php echo site_url('/') ?>"><strong>Fictional</strong> University</a></h1>
