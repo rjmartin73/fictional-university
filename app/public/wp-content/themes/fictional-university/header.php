3f49fda982f1d1618fdd2b5ad9725616a32cedd5
Ryan Martin <rjmartin73@gmail.com>
2018-10-09 20:17:19 -0600

    Navigation Menus 5-18

--- app/public/wp-content/themes/fictional-university/header.php
+++ app/public/wp-content/themes/fictional-university/header.php
@@ -14,8 +14,11 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
+          <!-- <?php wp_nav_menu(array(
+                'theme_location' => 'header-menu-location'
+              )); ?> -->
           <ul>
-            <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+            <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 50) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
             <li><a href="<?php echo site_url('/programs') ?>">Programs</a></li>
             <li><a href="<?php echo site_url('/events') ?>">Events</a></li>
             <li><a href="<?php echo site_url('/campuses') ?>">Campuses</a></li>
