cd83a9dbb729bd6d19cfa350ea5374ab799a6a37
Ryan Martin <rjmartin73@gmail.com>
2018-10-10 19:12:56 -0600

    Section 6.20 Blog posts(part 2)

--- app/public/wp-content/themes/fictional-university/index.php
+++ app/public/wp-content/themes/fictional-university/index.php
@@ -1,4 +1,5 @@
 <?php 
+  // header section
 get_header();
 ?>
 
@@ -14,7 +15,9 @@ get_header();
   </div>
 
   <div class="container container--narrow page-section">
-  <?php while (have_posts()) {
+  <?php
+    // while loop to retrieve the blog posts
+  while (have_posts()) {
     the_post(); ?>
     <div class="post-item">
       <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -22,16 +25,20 @@ get_header();
         <p>Posted by: <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?>  in <?php echo get_the_category_list('& ') ?> </p>
         </div>
         <div class="generic-content">
-          <?php the_excerpt(); ?>
-          <p> <a href="<?php the_permalink(); ?>" class="btn btn--blue">Continue reading &raquo;</a></p>
+          <?php the_excerpt(); // this retrieves the first part of a blog post ?>
+          <p> 
+            <a href="<?php the_permalink(); // this gets the link of the post ?>" class="btn btn--blue">Continue reading &raquo;</a>
+            </p>
         </div>
     </div>
     <?php
 
-  } ?>
-  </div>
+  }
+  echo paginate_links();
+  ?>
+    </div>
 
   <?php
-
+  // footer section
   get_footer();
   ?>
